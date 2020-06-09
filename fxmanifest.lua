ea2301fd66e80d7a2fb2e62dc199597d48e98704
Success <rjmxtechnology@gmail.com>
2020-06-08 20:26:05 -0500

    Add files to the repo
    -Textures compressed to work in FiveM and be under the 16MB limit for ytd's
    -Ran the files through Smallo's YBN YMAP mover poly edge fixer to fix vertexes

---
+++ fxmanifest.lua
@@ -0,0 +1,52 @@
+fx_version 'bodacious'
+game 'gta5'
+
+description 'San Fierro & Las Venturas Remastered (FiveM resource)'
+created_by 'GTA Belgium'
+original_source 'https://www.gta5-mods.com/maps/las-venturas-san-fierro-dlc-remastered'
+
+
+disclaimer1 'ALL credits and rights for this are due to GTA Belgium on GTA5-Mods'
+fivemresource 'Textures compressed by Success and put into a FiveM resource'
+disclaimer2 'I simply made this a FiveM resource that works (but it is not optimal by any means)'
+
+this_is_a_map 'yes'
+
+files {
+
+    --country-trees
+    'stream/sflv_metadata/country_trees.ytyp',
+    --las_venturas
+    'stream/sflv_metadata/las_venturas.ytyp',
+    --lv_props
+    'stream/sflv_metadata/lv_props.ytyp',
+    --lv_props2
+    'stream/sflv_metadata/lv_props2.ytyp',
+    --lv_trees
+    'stream/sflv_metadata/lv_trees.ytyp',
+    --san_fierro
+    'stream/sflv_metadata/san_fierro.ytyp',    
+    --sf_props
+    'stream/sflv_metadata/sf_props.ytyp',    
+    --vegasw_houses
+    'stream/sflv_metadata/vegasw_houses.ytyp',
+
+
+}
+
+--country-trees
+data_file 'DLC_ITYP_REQUEST' 'stream/sflv_metadata/country_trees.ytyp'
+--las_venturas
+data_file 'DLC_ITYP_REQUEST' 'stream/sflv_metadata/las_venturas.ytyp'
+--lv_props
+data_file 'DLC_ITYP_REQUEST' 'stream/sflv_metadata/lv_props.ytyp'
+--lv_props2
+data_file 'DLC_ITYP_REQUEST' 'stream/sflv_metadata/lv_props2.ytyp'
+--lv_trees
+data_file 'DLC_ITYP_REQUEST' 'stream/sflv_metadata/lv_trees.ytyp'
+--san_fierro
+data_file 'DLC_ITYP_REQUEST' 'stream/sflv_metadata/san_fierro.ytyp'
+--sf_props
+data_file 'DLC_ITYP_REQUEST' 'stream/sflv_metadata/sf_props.ytyp'
+--vegasw_houses
+data_file 'DLC_ITYP_REQUEST' 'stream/sflv_metadata/vegasw_houses.ytyp'
